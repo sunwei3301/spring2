62c18ed9c3b2cf929bd8a2718e5b08074c58c4e5
lenovo <18663700020@163.com>
2019-03-25 15:22:42 +0800

    22222

---
+++ myspring-1/src/main/java/com/example/demo/Myspring1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myspring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myspring1Application.class, args);
+	}
+
+}
